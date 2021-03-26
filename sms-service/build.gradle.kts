708109dd6918b947bc524595230d6ec95050fc1e
Oleg Yakovlev <oyakovlev@icerockdev.com>
2021-03-26 17:19:35 +0700

    Publish to Maven Central

--- sms-service/build.gradle.kts
+++ sms-service/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Base64
+import kotlin.text.String
 /*
  * Copyright 2020 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
  */
@@ -7,13 +9,14 @@ plugins {
     id("kotlin-kapt")
     id("maven-publish")
     id("java-library")
+    id("signing")
 }
 
 apply(plugin = "java")
 apply(plugin = "kotlin")
 
 group = "com.icerockdev.service"
-version = "0.1.0"
+version = "0.1.1"
 
 val sourcesJar by tasks.registering(Jar::class) {
     archiveClassifier.set("sources")
@@ -23,8 +26,6 @@ val sourcesJar by tasks.registering(Jar::class) {
 dependencies {
     // logging
     implementation("ch.qos.logback:logback-classic:${properties["logback_version"]}")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:${properties["coroutines_version"]}")
 
     // Twilio
     api("com.twilio.sdk:twilio:${properties["twilio_version"]}")
@@ -46,6 +47,7 @@ dependencies {
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
+    withJavadocJar()
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
@@ -59,18 +61,64 @@ repositories {
 }
 
 publishing {
-    repositories.maven("https://api.bintray.com/maven/icerockdev/backend/sms-service/;publish=1") {
-        name = "bintray"
+    repositories.maven("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/") {
+        name = "OSSRH"
 
         credentials {
-            username = System.getProperty("BINTRAY_USER")
-            password = System.getProperty("BINTRAY_KEY")
+            username = System.getenv("OSSRH_USER")
+            password = System.getenv("OSSRH_KEY")
         }
     }
     publications {
         register("mavenJava", MavenPublication::class) {
             from(components["java"])
             artifact(sourcesJar.get())
+            pom {
+                name.set("Sms service")
+                description.set("Sms sending service for kotlin")
+                url.set("https://github.com/icerockdev/sms-service")
+                licenses {
+                    license {
+                        url.set("https://github.com/icerockdev/sms-service/blob/master/LICENSE.md")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("YokiToki")
+                        name.set("Stanislav")
+                        email.set("skarakovski@icerockdev.com")
+                    }
+
+                    developer {
+                        id.set("AlexeiiShvedov")
+                        name.set("Alex Shvedov")
+                        email.set("ashvedov@icerockdev.com")
+                    }
+
+                    developer {
+                        id.set("oyakovlev")
+                        name.set("Oleg Yakovlev")
+                        email.set("oyakovlev@icerockdev.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:ssh://github.com/icerockdev/sms-service.git")
+                    developerConnection.set("scm:git:ssh://github.com/icerockdev/sms-service.git")
+                    url.set("https://github.com/icerockdev/sms-service")
+                }
+            }
+        }
+
+        signing {
+            val signingKeyId: String? = System.getenv("SIGNING_KEY_ID")
+            val signingPassword: String? = System.getenv("SIGNING_PASSWORD")
+            val signingKey: String? = System.getenv("SIGNING_KEY")?.let { base64Key ->
+                String(Base64.getDecoder().decode(base64Key))
+            }
+            useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+            sign(publishing.publications["mavenJava"])
         }
     }
 }
